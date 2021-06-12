77b0ab7258d8ec63366f985ae0d485e361974c73
Oleksandr98 <oleksandr.kropyvka@gmail.com>
2021-06-12 17:12:06 +0200

    cross origin cookies support

--- app/controllers/SocialAuthController.scala
+++ app/controllers/SocialAuthController.scala
@@ -3,6 +3,7 @@ package controllers
 
 import com.mohiva.play.silhouette.api.exceptions.ProviderException
 import com.mohiva.play.silhouette.impl.providers._
+import play.api.mvc.Cookie.SameSite
 import play.api.mvc.{Action, AnyContent, Cookie, Request}
 import play.filters.csrf.CSRF.Token
 import play.filters.csrf.{CSRF, CSRFAddToken}
@@ -23,10 +24,13 @@ class SocialAuthController @Inject()(scc: DefaultSilhouetteControllerComponents,
             _ <- authInfoRepository.save(profile.loginInfo, authInfo)
             authenticator <- authenticatorService.create(profile.loginInfo)
             value <- authenticatorService.init(authenticator)
-            result <- authenticatorService.embed(value, Redirect("https://ebiznes-clientside.azurewebsites.net"))
+            result <- authenticatorService.embed(value, Redirect(s"https://ebiznes-clientside.azurewebsites.net?csrfToken=${CSRF.getToken.get.value}")
+              .withHeaders("Access-Control-Allow-Origin" -> "https://ebiznes-clientside.azurewebsites.net",
+                "Access-Control-Allow-Credentials" -> "true"))
           } yield {
             val Token(name, value) = CSRF.getToken.get
-            result.withCookies(Cookie(name, value, httpOnly = false))
+            result.withCookies(
+              Cookie(name, value, httpOnly = false, sameSite = Option.apply(SameSite.None)))
           }
         }
       case _ => Future.failed(new ProviderException(s"Cannot authenticate with unexpected social provider $provider"))
